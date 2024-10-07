478963df43bb46996275fca7dc94a0f5eb793c3a
hf-chow <honfungchow@gmail.com>
2024-10-08 06:56:53 +0800

    add location_get

--- command_expore.go
+++ command_expore.go
@@ -2,52 +2,23 @@ package main
 
 import (
 	"errors"
+	"fmt"
 )
 
-type LocationArea struct {
-	ID							int
-	Name						string
-	Game_index					int
-	EncouterMethodRates		[]EncounterMethodRate
-	Location					Location
-	Names						[]string
-	PokemonEncounters			[]PokemonEncounters
-}
+func commandExplore(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("please provide a location")
+	}
+	name := args[0]
+	location, err := cfg.pokeapiClient.GetLocation(name)
+	if err != nil {
+		return err
+	}
 
-type EncounterMethodRate struct {
-	EncounterMethod		EncounterMethod
-	_					
-}
-
-type EncounterVersionDetail struct {
-	Rate			int
-	Version			EncounterVersionDetails
-}
-
-
-type EncounterMethod struct {
-	ID			int
-	Name		string
-	Names		[]string
-	Values		[]EncounterConditionValue
-}
-
-type EncounterConditionValue struct {
-	ID 			int
-	Name		string
-	Condition	EncounterCondition
-	Names		[]string
-}
-
-type EncounterCondition struct {
-	ID			int
-	Name		string
-	Names		[]string
-	Values		[]EncounterConditionValue
-}
-
-
-
-
-func commandExplore(cfg *config) error {
+	fmt.Printf("Exploring %s...\n", location.name)
+	fmt.Printf("Found Pokemon: ")
+	for _, enc := range location.PokemonEncounters {
+		fmt.Printf(" - %s\n", enc.Pokemon.Name)
+	}
+	return nil
 }
